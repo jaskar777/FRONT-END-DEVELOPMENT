3cea493e2bd6b85ac939db870046f45d9580ff20
jaskar777 <jaskari@karunya.edu.in>
2025-09-05 22:08:22 +0530

    added

---
+++ E-commerce/main.js
@@ -0,0 +1,14 @@
+var sidenav = document.querySelector(".side-navbar");
+
+
+function shownavbar() {
+    // sidenav.style.display = "block";
+    sidenav.style.left = "0";
+
+}
+
+function closenavbar() {
+    
+    // sidenav.style.display = "none";
+    sidenav.style.left = "-60%";
+}
